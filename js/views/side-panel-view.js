56c81a88b2e97217b278b15f0f83c2a0766681ac
nasitra <nasitra@users.noreply.github.com>
2015-09-15 19:27:24 +0900

    Refactor side-panel-view

--- js/views/side-panel-view.js
+++ js/views/side-panel-view.js
@@ -15,137 +15,10 @@
         autocapitalize: 'off',
         autocorrect: 'off',
         value: searchKeyword,
-        config: function(element, isInitialized) {
-          if (isInitialized)
-            return;
-
-          var isFocus = false;
-          if (dom.supportsTouch()) {
-            document.addEventListener('touchstart', function(event) {
-              if (event.target !== element)
-                element.blur();
-            });
-          } else {
-            element.addEventListener('blur', function() {
-              isFocus = false;
-            });
-            element.addEventListener('click', function() {
-              if (isFocus)
-                return;
-              isFocus = true;
-              if (element.selectionStart === element.selectionEnd)
-                element.select();
-            });
-          }
-
-          var changeListener = function(event) {
-            var cache = ctrl.searchKeyword();
-            var value = event.target.value;
-
-            if (value === cache)
-              return;
-
-            ctrl.dispatchEvent({
-              type: 'searchkeywordchange',
-              value: value
-            });
-          };
-          element.addEventListener('change', changeListener);
-          element.addEventListener('input', changeListener);
-        }
+        config: searchInputConfig.bind(ctrl)
       }),
       m('#piece_list', {
-        config: function(element, isInitialized) {
-          if (isInitialized)
-            return;
-
-          var isHeld;
-          var draggingNode;
-          var cloneOffset;
-          var isFirstDrag;
-          var isAutoClose;
-          var cloneNode;
-
-          var start = function() {
-            var pieceListContent = element.children[0];
-
-            cloneOffset = {
-              left: draggingNode.offsetLeft - pieceListContent.scrollLeft,
-              top: draggingNode.offsetTop - pieceListContent.scrollTop
-            };
-
-            isFirstDrag = true;
-            isAutoClose = false;
-
-            draggingNode.style.opacity = 0.6;
-
-            dom.removeKeyboardFocus();
-
-            cloneNode = draggingNode.cloneNode(true);
-            dom.addClass(cloneNode, 'drag');
-            document.body.appendChild(cloneNode);
-          };
-
-          dom.pointerEvent('.piece-list-item-container', {
-            onstart: function(event) {
-              isHeld = !dom.supportsTouch();
-              draggingNode = event.target;
-              if (!dom.supportsTouch())
-                start();
-            },
-            onhold: function() {
-              isHeld = true;
-              if (dom.supportsTouch())
-                start();
-            },
-            ondrag: function(event, dx, dy) {
-              if (!isHeld)
-                return;
-
-              event.preventDefault();
-              event.stopPropagation();
-
-              if (isFirstDrag) {
-                isFirstDrag = false;
-
-                if (window.innerWidth < 358) {
-                  isAutoClose = true;
-                  ctrl.dispatchEvent({type: 'close'});
-                }
-
-                cloneNode.style.left = cloneOffset.left + 'px';
-                cloneNode.style.top = cloneOffset.top + 'px';
-              }
-
-              dom.translate(cloneNode, dx, dy);
-            },
-            ondragend: function(event, dx, dy) {
-              if (!isHeld)
-                return;
-
-              var pageX = cloneOffset.left + dx;
-              var pageY = cloneOffset.top + dy;
-              var dragEndEvent = {
-                type: 'dragend',
-                pageX: pageX,
-                pageY: pageY,
-                pieceSrc: cloneNode.getAttribute('data-piece-src'),
-                pieceLabel: cloneNode.children[0].textContent
-              };
-              ctrl.dispatchEvent(dragEndEvent);
-            },
-            onend: function() {
-              if (!isHeld)
-                return;
-
-              draggingNode.style.cssText = '';
-              document.body.removeChild(cloneNode);
-
-              if (isAutoClose)
-                ctrl.dispatchEvent({type: 'open'});
-            }
-          });
-        }
+        config: pieceListConfig.bind(ctrl)
       }, [
         m('#piece_list_content', pieceList.map(function(item) {
           return m('.piece-list-item', [
@@ -161,35 +34,174 @@
       ]),
       m('#divider', [
         m('#divider_icon', {
-          config: function(element, isInitialized) {
-            if (isInitialized)
-              return;
-
-            dom.pointerEvent('#divider_icon', {
-              ontap: function() {
-                ctrl.dispatchEvent({type: 'toggle'});
-                dom.setCursor('default');
-                dom.removeClass(element, 'tap');
-              },
-              onstart: function(event) {
-                event.preventDefault();
-                dom.addClass(element, 'tap');
-                dom.removeKeyboardFocus();
-                dom.setCursor('pointer');
-              },
-              onout: function() {
-                dom.removeClass(element, 'tap');
-                dom.setCursor('default');
-              },
-              onover: function() {
-                dom.addClass(element, 'tap');
-                dom.setCursor('pointer');
-              }
-            });
-          }
+          config: dividerIconConfig.bind(ctrl)
         })
       ])
     ]);
+  };
+
+  var searchInputConfig = function(element, isInitialized) {
+    if (isInitialized)
+      return;
+
+    var ctrl = this;
+
+    var isFocus = false;
+    if (dom.supportsTouch()) {
+      document.addEventListener('touchstart', function(event) {
+        if (event.target !== element)
+          element.blur();
+      });
+    } else {
+      element.addEventListener('blur', function() {
+        isFocus = false;
+      });
+      element.addEventListener('click', function() {
+        if (isFocus)
+          return;
+        isFocus = true;
+        if (element.selectionStart === element.selectionEnd)
+          element.select();
+      });
+    }
+
+    var changeListener = function(event) {
+      var cache = ctrl.searchKeyword();
+      var value = event.target.value;
+
+      if (value === cache)
+        return;
+
+      ctrl.dispatchEvent({
+        type: 'searchkeywordchange',
+        value: value
+      });
+    };
+    element.addEventListener('change', changeListener);
+    element.addEventListener('input', changeListener);
+  };
+
+  var pieceListConfig = function(element, isInitialized) {
+    if (isInitialized)
+      return;
+
+    var ctrl = this;
+
+    var isHeld;
+    var draggingNode;
+    var cloneOffset;
+    var isFirstDrag;
+    var isAutoClose;
+    var cloneNode;
+
+    var start = function() {
+      var pieceListContent = element.children[0];
+
+      cloneOffset = {
+        left: draggingNode.offsetLeft - pieceListContent.scrollLeft,
+        top: draggingNode.offsetTop - pieceListContent.scrollTop
+      };
+
+      isFirstDrag = true;
+      isAutoClose = false;
+
+      draggingNode.style.opacity = 0.6;
+
+      dom.removeKeyboardFocus();
+
+      cloneNode = draggingNode.cloneNode(true);
+      dom.addClass(cloneNode, 'drag');
+      document.body.appendChild(cloneNode);
+    };
+
+    dom.pointerEvent('.piece-list-item-container', {
+      onstart: function(event) {
+        isHeld = !dom.supportsTouch();
+        draggingNode = event.target;
+        if (!dom.supportsTouch())
+          start();
+      },
+      onhold: function() {
+        isHeld = true;
+        if (dom.supportsTouch())
+          start();
+      },
+      ondrag: function(event, dx, dy) {
+        if (!isHeld)
+          return;
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (isFirstDrag) {
+          isFirstDrag = false;
+
+          if (window.innerWidth < 358) {
+            isAutoClose = true;
+            ctrl.dispatchEvent({type: 'close'});
+          }
+
+          cloneNode.style.left = cloneOffset.left + 'px';
+          cloneNode.style.top = cloneOffset.top + 'px';
+        }
+
+        dom.translate(cloneNode, dx, dy);
+      },
+      ondragend: function(event, dx, dy) {
+        if (!isHeld)
+          return;
+
+        var pageX = cloneOffset.left + dx;
+        var pageY = cloneOffset.top + dy;
+        var dragEndEvent = {
+          type: 'dragend',
+          pageX: pageX,
+          pageY: pageY,
+          pieceSrc: cloneNode.getAttribute('data-piece-src'),
+          pieceLabel: cloneNode.children[0].textContent
+        };
+        ctrl.dispatchEvent(dragEndEvent);
+      },
+      onend: function() {
+        if (!isHeld)
+          return;
+
+        draggingNode.style.cssText = '';
+        document.body.removeChild(cloneNode);
+
+        if (isAutoClose)
+          ctrl.dispatchEvent({type: 'open'});
+      }
+    });
+  };
+
+  var dividerIconConfig = function(element, isInitialized) {
+    if (isInitialized)
+      return;
+
+    var ctrl = this;
+
+    dom.pointerEvent('#divider_icon', {
+      ontap: function() {
+        ctrl.dispatchEvent({type: 'toggle'});
+        dom.setCursor('default');
+        dom.removeClass(element, 'tap');
+      },
+      onstart: function(event) {
+        event.preventDefault();
+        dom.addClass(element, 'tap');
+        dom.removeKeyboardFocus();
+        dom.setCursor('pointer');
+      },
+      onout: function() {
+        dom.removeClass(element, 'tap');
+        dom.setCursor('default');
+      },
+      onover: function() {
+        dom.addClass(element, 'tap');
+        dom.setCursor('pointer');
+      }
+    });
   };
 
   if (typeof module !== 'undefined' && module.exports)
