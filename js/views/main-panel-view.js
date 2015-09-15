74bbd55a78369238e5138f2ad0293ae9637502ea
nasitra <nasitra@users.noreply.github.com>
2015-09-15 23:28:59 +0900

    Refactor main-panel-view

--- js/views/main-panel-view.js
+++ js/views/main-panel-view.js
@@ -5,48 +5,56 @@
 
   var mainPanelView = function(ctrl) {
     return m('#main_panel', {
-      config: function(element, isInitialized) {
-        if (isInitialized)
-          return;
-
-        ctrl.dispatchEvent({
-          type: 'init',
-          element: element
-        });
-
-        // board event
-        boardEvent.initialize(element);
-        boardEvent.loadURLHash();
-      }
+      config: mainPanelConfig.bind(ctrl)
     }, [
       boardView(ctrl.boardController),
       m('.retainer', {
-        config: function(element, isInitialized) {
-          if (!isInitialized)
-            return;
-
-          var mainPanelElement = ctrl.element();
-
-          var scrollLeft = mainPanelElement.scrollLeft;
-          var scrollTop = mainPanelElement.scrollTop;
-
-          dom.addClass(element, 'hide');
-
-          var clientWidth = mainPanelElement.clientWidth;
-          var clientHeight = mainPanelElement.clientHeight;
-          var scrollWidth = mainPanelElement.scrollWidth;
-          var scrollHeight = mainPanelElement.scrollHeight;
-          if (scrollWidth > clientWidth || scrollHeight > clientHeight) {
-            var x = scrollWidth + (scrollWidth > clientWidth ? 46 : -1);
-            var y = scrollHeight + (scrollHeight > clientHeight ? 46 : -1);
-            dom.translate(element, x, y);
-            dom.removeClass(element, 'hide');
-            mainPanelElement.scrollLeft = scrollLeft;
-            mainPanelElement.scrollTop = scrollTop;
-          }
-        }
+        config: retainerConfig.bind(ctrl)
       })
     ]);
+  };
+
+  var mainPanelConfig = function(element, isInitialized) {
+    if (isInitialized)
+      return;
+
+    var ctrl = this;
+
+    ctrl.dispatchEvent({
+      type: 'init',
+      element: element
+    });
+
+    // board event
+    boardEvent.initialize(element);
+    boardEvent.loadURLHash();
+  };
+
+  var retainerConfig = function(element, isInitialized) {
+    if (!isInitialized)
+      return;
+
+    var ctrl = this;
+
+    var mainPanelElement = ctrl.element();
+
+    var scrollLeft = mainPanelElement.scrollLeft;
+    var scrollTop = mainPanelElement.scrollTop;
+
+    dom.addClass(element, 'hide');
+
+    var clientWidth = mainPanelElement.clientWidth;
+    var clientHeight = mainPanelElement.clientHeight;
+    var scrollWidth = mainPanelElement.scrollWidth;
+    var scrollHeight = mainPanelElement.scrollHeight;
+    if (scrollWidth > clientWidth || scrollHeight > clientHeight) {
+      var x = scrollWidth + (scrollWidth > clientWidth ? 46 : -1);
+      var y = scrollHeight + (scrollHeight > clientHeight ? 46 : -1);
+      dom.translate(element, x, y);
+      dom.removeClass(element, 'hide');
+      mainPanelElement.scrollLeft = scrollLeft;
+      mainPanelElement.scrollTop = scrollTop;
+    }
   };
 
   if (typeof module !== 'undefined' && module.exports)
