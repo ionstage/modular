8f4d1b7a776a771812a61f27fb9e656653ffaed7
nasitra <nasitra@users.noreply.github.com>
2016-05-04 15:07:29 +0900

    Add property to access element of module-component

--- js/components/module-component.js
+++ js/components/module-component.js
@@ -4,8 +4,10 @@
   var helper = app.helper || require('../helper.js');
   var Component = app.Component || require('./component.js');
 
-  var ModuleComponent = helper.inherits(function() {
+  var ModuleComponent = helper.inherits(function(props) {
     ModuleComponent.super_.call(this);
+
+    this.element = this.prop(props.element);
   }, Component);
 
   if (typeof module !== 'undefined' && module.exports)
