a742690b8eb5dbb480addc618a7627816edf33e1
nasitra <nasitra@users.noreply.github.com>
2016-06-02 18:46:33 +0900

    Add an implementation of the circuit-element

--- js/models/circuit-element.js
+++ js/models/circuit-element.js
@@ -1,7 +1,52 @@
 (function(app) {
   'use strict';
 
-  var CircuitElement = function() {};
+  var circuit = require('circuit');
+
+  var Wrapper = function(self) {
+    this.unwrap = Wrapper.unwrap.bind(self);
+  };
+
+  Wrapper.unwrap = function(key) {
+    if (key === Wrapper.KEY)
+      return this;
+  };
+
+  Wrapper.KEY = {};
+
+  var CircuitElementMember = function(props) {
+    var name = props.name;
+    var arg = props.arg;
+    var type = props.type;
+
+    if (typeof type === 'undefined' || (type !== 'prop' && type !== 'event'))
+      type = (name.indexOf('on') === 0) ? 'event' : 'prop';
+
+    this.name = name;
+    this.type = type;
+    this.callee = circuit[type](arg);
+    this.sources = [];
+    this.targets = [];
+    this.wrapper = new Wrapper(this);
+  };
+
+  var CircuitElement = function(members) {
+    var memberTable = {};
+    var names = [];
+
+    members.slice().reverse().forEach(function(props) {
+      var name = props.name;
+
+      if (name in memberTable)
+        return;
+
+      memberTable[name] = new CircuitElementMember(props);
+      names.unshift(name);
+    });
+
+    this.memberTable = memberTable;
+    this.names = names;
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = CircuitElement;
