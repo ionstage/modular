c6fb3a672abde49427421c2f3a572bf5537a299f
nasitra <nasitra@users.noreply.github.com>
2016-06-29 18:54:04 +0900

    Move the module on top of others when pointing the module element

--- js/components/module-container.js
+++ js/components/module-container.js
@@ -3,6 +3,7 @@
 
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Module = app.Module || require('./module.js');
 
   var ModuleContainer = helper.inherits(function(props) {
@@ -12,6 +13,11 @@
     this.element = this.prop(props.element);
 
     this.deleter = ModuleContainer.prototype.deleter.bind(this);
+
+    new dom.Draggable({
+      element: this.element(),
+      onstart: ModuleContainer.prototype.onstart.bind(this)
+    });
   }, jCore.Component);
 
   ModuleContainer.prototype.loadModule = function(props) {
@@ -34,6 +40,24 @@
 
     modules.splice(index, 1);
     modules.push(module);
+  };
+
+  ModuleContainer.prototype.onstart = function(x, y, event) {
+    var modules = this.modules();
+    var target = dom.target(event);
+
+    var module = modules.filter(function(module) {
+      return dom.contains(module.element(), target);
+    })[0];
+
+    if (!module)
+      return;
+
+    // move the target module on top of others
+    this.toFront(module);
+    modules.forEach(function(module, index) {
+      module.zIndex(index);
+    });
   };
 
   ModuleContainer.prototype.deleter = function(module) {
