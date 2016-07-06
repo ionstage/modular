2600e8eb06faa60d60b1308127f525beb8bda76e
nasitra <nasitra@users.noreply.github.com>
2016-07-06 18:22:32 +0900

    Make skeleton element of module-wire

--- js/components/module-wire.js
+++ js/components/module-wire.js
@@ -3,12 +3,37 @@
 
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
 
   var ModuleWire = helper.inherits(function() {
     ModuleWire.super_.call(this);
 
     this.element = this.prop(null);
+    this.parentElement = this.prop(null);
   }, jCore.Component);
+
+  ModuleWire.prototype.redraw = function() {
+    var element = this.element();
+    var parentElement = this.parentElement();
+
+    if (!parentElement && !element)
+      return;
+
+    // add element
+    if (parentElement && !element) {
+      element = dom.el('<div>');
+      this.element(element);
+      dom.append(parentElement, element);
+      return;
+    }
+
+    // remove element
+    if (!parentElement && element) {
+      dom.remove(element);
+      this.element(null);
+      return;
+    }
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = ModuleWire;
