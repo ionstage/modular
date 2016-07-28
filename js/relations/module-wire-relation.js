aae2871cec247e332b05337bf45802a10f214afc
nasitra <nasitra@users.noreply.github.com>
2016-07-28 18:42:42 +0900

    Enable updating a module-wire position with a bound port

--- js/relations/module-wire-relation.js
+++ js/relations/module-wire-relation.js
@@ -3,10 +3,41 @@
 
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
+  var ModulePort = app.ModulePort || require('./module-port.js');
 
-  var ModuleWireRelation = helper.inherits(function() {
+  var ModuleWireRelation = helper.inherits(function(props) {
     ModuleWireRelation.super_.call(this);
+
+    this.type = this.prop(props.type);
+    this.module = this.prop(props.module);
+    this.port = this.prop(props.port);
+    this.wire = this.prop(props.wire);
   }, jCore.Relation);
+
+  ModuleWireRelation.prototype.update = function() {
+    var port = this.port();
+
+    if (!port.visible())
+      return;
+
+    var type = this.type();
+    var module = this.module();
+    var wire = this.wire();
+
+    var offsetX = ({
+      source: ModulePort.SOCKET_OFFSET_X,
+      target: ModulePort.PLUG_OFFSET_X
+    })[type];
+
+    var x = module.x() + offsetX;
+    var y = module.y() + module.portListTop() + port.top() + port.height() / 2;
+
+    wire[type + 'X'](x);
+    wire[type + 'Y'](y);
+  };
+
+  ModuleWireRelation.TYPE_SOURCE = 'source';
+  ModuleWireRelation.TYPE_TARGET = 'target';
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = ModuleWireRelation;
