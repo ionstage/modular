5dcbf8c82bec5ccc94a1a3ee209697bfae0bc45c
nasitra <nasitra@users.noreply.github.com>
2016-11-13 16:40:05 +0900

    Add property to access element of the content header

--- js/components/content-header.js
+++ js/components/content-header.js
@@ -4,8 +4,10 @@
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
 
-  var ContentHeader = helper.inherits(function() {
+  var ContentHeader = helper.inherits(function(props) {
     ContentHeader.super_.call(this);
+
+    this.element = this.prop(props.element);
   }, jCore.Component);
 
   if (typeof module !== 'undefined' && module.exports)
