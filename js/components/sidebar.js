ab6af99741e142832d75c3d255c06a4a6bce4184
nasitra <nasitra@users.noreply.github.com>
2016-11-14 18:06:51 +0900

    Add property to access element of the sidebar

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -4,8 +4,10 @@
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
 
-  var Sidebar = helper.inherits(function() {
+  var Sidebar = helper.inherits(function(props) {
     Sidebar.super_.call(this);
+
+    this.element = this.prop(props.element);
   }, jCore.Component);
 
   if (typeof module !== 'undefined' && module.exports)
