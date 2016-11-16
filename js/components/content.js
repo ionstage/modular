6924543534a93d41f2220ad94c71d160c02b3c1f
nasitra <nasitra@users.noreply.github.com>
2016-11-17 07:28:28 +0900

    Add property to access element of the content

--- js/components/content.js
+++ js/components/content.js
@@ -4,8 +4,10 @@
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
 
-  var Content = helper.inherits(function() {
+  var Content = helper.inherits(function(props) {
     Content.super_.call(this);
+
+    this.element = this.prop(props.element);
   }, jCore.Component);
 
   if (typeof module !== 'undefined' && module.exports)
