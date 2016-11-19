1058429951a36d6033d701d60a67327695e2b330
nasitra <nasitra@users.noreply.github.com>
2016-11-20 08:29:18 +0900

    Add method to access an element of sidebar-toggle-button

--- js/components/content-header.js
+++ js/components/content-header.js
@@ -3,12 +3,17 @@
 
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
 
   var ContentHeader = helper.inherits(function(props) {
     ContentHeader.super_.call(this);
 
     this.element = this.prop(props.element);
   }, jCore.Component);
+
+  ContentHeader.prototype.sidebarToggleButtonElement = function() {
+    return dom.child(this.element(), 0);
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = ContentHeader;
