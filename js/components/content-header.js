a32b7d2d669e633847a26e06cc8e91732dda389b
nasitra <nasitra@users.noreply.github.com>
2016-11-21 18:18:37 +0900

    Add property of sidebar-toggle-type

--- js/components/content-header.js
+++ js/components/content-header.js
@@ -8,12 +8,16 @@
   var ContentHeader = helper.inherits(function(props) {
     ContentHeader.super_.call(this);
 
+    this.sidebarToggleType = this.prop(ContentHeader.SIDEBAR_TOGGLE_TYPE_COLLAPSE);
     this.element = this.prop(props.element);
   }, jCore.Component);
 
   ContentHeader.prototype.sidebarToggleButtonElement = function() {
     return dom.child(this.element(), 0);
   };
+
+  ContentHeader.SIDEBAR_TOGGLE_TYPE_COLLAPSE = 'collapse';
+  ContentHeader.SIDEBAR_TOGGLE_TYPE_EXPAND = 'expand';
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = ContentHeader;
