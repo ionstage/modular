03435337f6cd54afe3b367882ac398b6cbe89afe
nasitra <nasitra@users.noreply.github.com>
2016-11-26 09:39:14 +0900

    Make the code of sidebar-toggle-button a separate file

---
+++ js/components/sidebar-toggle-button.js
@@ -0,0 +1,80 @@
+(function(app) {
+  'use strict';
+
+  var jCore = require('jcore');
+  var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
+
+  var SidebarToggleButton = helper.inherits(function(props) {
+    SidebarToggleButton.super_.call(this);
+
+    this.type = this.prop(SidebarToggleButton.TYPE_COLLAPSE);
+    this.disabled = this.prop(false);
+    this.element = this.prop(props.element);
+    this.cache = this.prop({});
+
+    this.collapser = props.collapser;
+    this.expander = props.expander;
+  }, jCore.Component);
+
+  SidebarToggleButton.prototype.toggler = (function() {
+    var map = { collapse: 'collapser', expand: 'expander' };
+    return function() {
+      return this[map[this.type()]]();
+    };
+  })();
+
+  SidebarToggleButton.prototype.switchType = (function() {
+    var map = { collapse: 'expand', expand: 'collapse' };
+    return function() {
+      this.type(map[this.type()]);
+    };
+  })();
+
+  SidebarToggleButton.prototype.registerClickListener = function() {
+    dom.on(this.element(), 'click', function() {
+      this.disabled(true);
+      this.toggler().then(function() {
+        this.switchType();
+        this.disabled(false);
+      }.bind(this)).catch(function() {
+        this.disabled(false);
+      }.bind(this));
+    }.bind(this));
+  };
+
+  SidebarToggleButton.prototype.redraw = function() {
+    this.redrawType();
+    this.redrawDisabled();
+  };
+
+  SidebarToggleButton.prototype.redrawType = function() {
+    var type = this.type();
+    var cache = this.cache();
+
+    if (type === cache.type)
+      return;
+
+    dom.data(this.element(), 'type', type);
+    cache.type = type;
+  };
+
+  SidebarToggleButton.prototype.redrawDisabled = function() {
+    var disabled = this.disabled();
+    var cache = this.cache();
+
+    if (disabled === cache.disabled)
+      return;
+
+    dom.disabled(this.element(), disabled);
+    cache.disabled = disabled;
+  };
+
+  SidebarToggleButton.TYPE_COLLAPSE = 'collapse';
+  SidebarToggleButton.TYPE_EXPAND = 'expand';
+
+  if (typeof module !== 'undefined' && module.exports)
+    module.exports = SidebarToggleButton;
+  else
+    app.SidebarToggleButton = SidebarToggleButton;
+})(this.app || (this.app = {}));
