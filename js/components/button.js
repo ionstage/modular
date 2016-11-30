30db0bd74b5f7bec451a0da19a1cc1633d51d19f
nasitra <nasitra@users.noreply.github.com>
2016-11-30 18:20:45 +0900

    Make 'active' state of a button

--- js/components/button.js
+++ js/components/button.js
@@ -3,12 +3,30 @@
 
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
 
   var Button = helper.inherits(function(props) {
     Button.super_.call(this);
 
+    this.isActive = this.prop(false);
     this.element = this.prop(props.element);
+    this.cache = this.prop({});
   }, jCore.Component);
+
+  Button.prototype.redraw = function() {
+    this.redrawIsActive();
+  };
+
+  Button.prototype.redrawIsActive = function() {
+    var isActive = this.isActive();
+    var cache = this.cache();
+
+    if (isActive === cache.isActive)
+      return;
+
+    dom.toggleClass(this.element(), 'active', isActive);
+    cache.isActive = isActive;
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = Button;
