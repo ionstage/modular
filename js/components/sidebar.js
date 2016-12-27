74a9efc0a1c633a12513140b93cfa53221fd6088
nasitra <nasitra@users.noreply.github.com>
2016-12-27 19:59:31 +0900

    Add method to access search-input-element of the sidebar

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -3,12 +3,17 @@
 
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
 
   var Sidebar = helper.inherits(function(props) {
     Sidebar.super_.call(this);
 
     this.element = this.prop(props.element);
   }, jCore.Component);
+
+  Sidebar.prototype.searchInputElement = function() {
+    return dom.child(this.element(), 0, 0);
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = Sidebar;
