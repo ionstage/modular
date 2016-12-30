77db331fcb9860031eb408a85be900cd4d5b39bf
nasitra <nasitra@users.noreply.github.com>
2016-12-30 09:31:31 +0900

    Add method to access an element of the content header

--- js/components/content.js
+++ js/components/content.js
@@ -3,12 +3,17 @@
 
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
 
   var Content = helper.inherits(function(props) {
     Content.super_.call(this);
 
     this.element = this.prop(props.element);
   }, jCore.Component);
+
+  Content.prototype.contentHeaderElement = function() {
+    return dom.child(this.element(), 0);
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = Content;
