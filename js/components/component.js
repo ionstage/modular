62b16638f3ee72fc23c6b8ab8b0bdf835318822a
nasitra <nasitra@users.noreply.github.com>
2017-01-01 09:55:37 +0900

    Add method to update a state of a component

--- js/components/component.js
+++ js/components/component.js
@@ -3,10 +3,26 @@
 
   var jCore = require('jcore');
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
 
-  var Component = helper.inherits(function() {
+  var Component = helper.inherits(function(props) {
     Component.super_.call(this);
+
+    this.element = this.prop(props.element);
+    this.cache = this.prop({});
   }, jCore.Component);
+
+  Component.prototype.redrawState = function(key, className) {
+    var cache = this.cache();
+    var value = this[key]();
+
+    if (value === cache[key]) {
+      return;
+    }
+
+    dom.toggleClass(this.element(), className, value);
+    cache[key] = value;
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Component;
