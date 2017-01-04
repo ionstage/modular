0ce710e099dff8df1285b679cd0835f3bacdd46a
nasitra <nasitra@users.noreply.github.com>
2017-01-04 09:36:47 +0900

    Make skeleton element of sidebar-module

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -10,7 +10,32 @@
 
     this.title = this.prop(props.title);
     this.content = this.prop(props.content);
+    this.parentElement = this.prop(null);
   }, Component);
+
+  SidebarModule.prototype.redraw = function() {
+    var element = this.element();
+    var parentElement = this.parentElement();
+
+    if (!parentElement && !element) {
+      return;
+    }
+
+    // add element
+    if (parentElement && !element) {
+      element = dom.el('<div>');
+      this.element(element);
+      dom.append(parentElement, element);
+      return;
+    }
+
+    // remove element
+    if (!parentElement && element) {
+      dom.remove(element);
+      this.element(null);
+      return;
+    }
+  };
 
   var Sidebar = helper.inherits(function(props) {
     Sidebar.super_.call(this, props);
