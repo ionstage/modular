b78b8344cb320afb1b1b6739d2e10e2396893cd1
nasitra <nasitra@users.noreply.github.com>
2017-01-06 18:21:31 +0900

    Make the code of sidebar-module a separate file

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -5,49 +5,6 @@
   var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
-  var SidebarModule = helper.inherits(function(props) {
-    SidebarModule.super_.call(this, props);
-
-    this.title = this.prop(props.title);
-    this.content = this.prop(props.content);
-    this.parentElement = this.prop(null);
-  }, Component);
-
-  SidebarModule.prototype.render = function() {
-    var element = dom.el('<div>');
-    dom.addClass(element, 'module');
-    dom.html(element, SidebarModule.TEMPLATE_HTML);
-    return element;
-  };
-
-  SidebarModule.prototype.redraw = function() {
-    var element = this.element();
-    var parentElement = this.parentElement();
-
-    if (!parentElement && !element) {
-      return;
-    }
-
-    // add element
-    if (parentElement && !element) {
-      this.element(this.render());
-      dom.append(parentElement, this.element());
-      return;
-    }
-
-    // remove element
-    if (!parentElement && element) {
-      dom.remove(element);
-      this.element(null);
-      return;
-    }
-  };
-
-  SidebarModule.TEMPLATE_HTML = [
-    '<div class="module-header"></div>',
-    '<div class="module-content"></div>',
-  ].join('');
-
   var Sidebar = helper.inherits(function(props) {
     Sidebar.super_.call(this, props);
   }, Component);
