c91f8452eb34ed62e6d06e7b607ea8a7fd937bb6
nasitra <nasitra@users.noreply.github.com>
2017-01-09 09:47:31 +0900

    Add method to append module to the sidebar-content

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -4,6 +4,7 @@
   var helper = app.helper || require('../helper.js');
   var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
+  var SidebarModule = app.SidebarModule || require('./sidebar-module.js');
 
   var Sidebar = helper.inherits(function(props) {
     Sidebar.super_.call(this, props);
@@ -15,6 +16,11 @@
 
   Sidebar.prototype.contentElement = function() {
     return dom.child(this.element(), 1);
+  };
+
+  Sidebar.prototype.appendModule = function(props) {
+    var module = new SidebarModule(props);
+    module.parentElement(this.contentElement());
   };
 
   if (typeof module !== 'undefined' && module.exports) {
