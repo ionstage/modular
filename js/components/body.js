f5b0bda483d2f325b76341afe6c6fd0ef491bd15
nasitra <nasitra@users.noreply.github.com>
2017-02-12 07:57:33 +0900

    Add method to calculate a keyword score of module-data

--- js/components/body.js
+++ js/components/body.js
@@ -20,6 +20,42 @@
     return this.packageName + '/' + this.src;
   };
 
+  ModuleData.prototype.keywordScore = function(keyword) {
+    if (!keyword) {
+      return 0;
+    }
+
+    keyword = keyword.toLowerCase();
+
+    var label = this.label.toLowerCase();
+    if (label.indexOf(keyword) === 0) {
+      return 1;
+    }
+
+    var tags = this.tags;
+    for (var i = 0, len = tags.length; i < len; i++) {
+      var tag = tags[i].toLowerCase();
+      if (tag.indexOf(keyword) !== -1) {
+        return 0.75;
+      }
+    }
+
+    if (keyword.length < 2) {
+      return 0;
+    }
+
+    if (label.indexOf(keyword) !== -1) {
+      return 0.5;
+    }
+
+    var description = this.description.toLowerCase();
+    if (description.indexOf(keyword) !== -1) {
+      return 0.25;
+    }
+
+    return 0;
+  };
+
   var ModuleDataCollection = function() {
     this.data = {};
   };
