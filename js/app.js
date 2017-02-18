887e5b6bdca1746c7c847c7d1a8b04325ba421e6
nasitra <nasitra@users.noreply.github.com>
2017-02-18 10:53:47 +0900

    Renew application interface

--- js/app.js
+++ js/app.js
@@ -1,28 +1,14 @@
 (function(app) {
   'use strict';
-  var m = require('mithril');
-  var SidePanelComponent = app.SidePanelComponent || require('./components/side-panel-component.js');
-  var MainPanelComponent = app.MainPanelComponent || require('./components/main-panel-component.js');
 
-  var controller = function() {
-    var noop = function() {};
-    var panelEvent = this.panelEvent = {
-      addPiece: noop,
-      ondragend: function(event) {
-        panelEvent.addPiece(event.pageX, event.pageY, event.pieceLabel, event.pieceSrc);
-      }
-    };
-  };
+  var dom = app.dom || require('./dom.js');
+  var Body = app.Body || require('./components/body.js');
 
-  var view = function(ctrl) {
-    return [
-      m.component(SidePanelComponent, ctrl.panelEvent),
-      m.component(MainPanelComponent, ctrl.panelEvent)
-    ];
-  };
+  app.body = new Body();
 
-  m.mount(document.getElementById('container'), {
-    controller: controller,
-    view: view
-  });
+  // for modular-module
+  var global = dom.global();
+  if (!global.app) {
+    global.app = {};
+  }
 })(this.app || (this.app = {}));
