dd3689c3cbe2a2b59493f434b6a29b55b70a8caf
nasitra <nasitra@users.noreply.github.com>
2017-03-09 18:55:34 +0900

    Make the code of module-entry-collection a separate file

---
+++ js/collections/module-entry-collection.js
@@ -0,0 +1,76 @@
+(function(app) {
+  'use strict';
+
+  var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
+  var ModuleEntry = app.ModuleEntry || require('../models/module-entry.js');
+
+  var ModuleEntryCollection = function() {
+    this.data = {};
+  };
+
+  ModuleEntryCollection.prototype.packageNamesUrl = function() {
+    return 'modular_modules/index.json';
+  };
+
+  ModuleEntryCollection.prototype.moduleEntriesUrl = function(packageName) {
+    return [
+      'modular_modules/',
+      packageName.split('/').map(function(s) {
+        return encodeURIComponent(s);
+      }).join('/'),
+      '/index.json',
+    ].join('');
+  };
+
+  ModuleEntryCollection.prototype.load = function() {
+    return this.loadPackageNames().then(function(packageNames) {
+      return Promise.all(packageNames.map(function(packageName) {
+        return this.loadModuleEntries(packageName).then(function(moduleEntries) {
+          moduleEntries.forEach(function(moduleEntry) {
+            this.data[moduleEntry.key()] = moduleEntry;
+          }.bind(this));
+        }.bind(this));
+      }.bind(this)));
+    }.bind(this));
+  };
+
+  ModuleEntryCollection.prototype.loadPackageNames = function() {
+    return dom.ajax({
+      type: 'GET',
+      url: this.packageNamesUrl(),
+    }).then(function(text) {
+      return JSON.parse(text);
+    });
+  };
+
+  ModuleEntryCollection.prototype.loadModuleEntries = function(packageName) {
+    return dom.ajax({
+      type: 'GET',
+      url: this.moduleEntriesUrl(packageName),
+    }).then(function(text) {
+      return JSON.parse(text).map(function(props) {
+        return new ModuleEntry(helper.extend(helper.clone(props), { packageName: packageName }));
+      });
+    });
+  };
+
+  ModuleEntryCollection.prototype.get = function(key) {
+    return this.data[key] || null;
+  };
+
+  ModuleEntryCollection.prototype.search = function(keyword) {
+    return helper.sortBy(helper.values(this.data).filter(function(moduleEntry) {
+      return (moduleEntry.keywordScore(keyword) !== 0);
+    }), function(moduleEntry) {
+      // sort in descending order
+      return -(moduleEntry.keywordScore(keyword));
+    });
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = ModuleEntryCollection;
+  } else {
+    app.ModuleEntryCollection = ModuleEntryCollection;
+  }
+})(this.app || (this.app = {}));
--- js/components/body.js
+++ js/components/body.js
@@ -5,71 +5,8 @@
   var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
   var Content = app.Content || require('./content.js');
-  var ModuleEntry = app.ModuleEntry || require('../models/module-entry.js');
+  var ModuleEntryCollection = app.ModuleEntryCollection || require('../collections/module-entry-collection.js');
   var Sidebar = app.Sidebar || require('./sidebar.js');
-
-  var ModuleEntryCollection = function() {
-    this.data = {};
-  };
-
-  ModuleEntryCollection.prototype.packageNamesUrl = function() {
-    return 'modular_modules/index.json';
-  };
-
-  ModuleEntryCollection.prototype.moduleEntriesUrl = function(packageName) {
-    return [
-      'modular_modules/',
-      packageName.split('/').map(function(s) {
-        return encodeURIComponent(s);
-      }).join('/'),
-      '/index.json',
-    ].join('');
-  };
-
-  ModuleEntryCollection.prototype.load = function() {
-    return this.loadPackageNames().then(function(packageNames) {
-      return Promise.all(packageNames.map(function(packageName) {
-        return this.loadModuleEntries(packageName).then(function(moduleEntries) {
-          moduleEntries.forEach(function(moduleEntry) {
-            this.data[moduleEntry.key()] = moduleEntry;
-          }.bind(this));
-        }.bind(this));
-      }.bind(this)));
-    }.bind(this));
-  };
-
-  ModuleEntryCollection.prototype.loadPackageNames = function() {
-    return dom.ajax({
-      type: 'GET',
-      url: this.packageNamesUrl(),
-    }).then(function(text) {
-      return JSON.parse(text);
-    });
-  };
-
-  ModuleEntryCollection.prototype.loadModuleEntries = function(packageName) {
-    return dom.ajax({
-      type: 'GET',
-      url: this.moduleEntriesUrl(packageName),
-    }).then(function(text) {
-      return JSON.parse(text).map(function(props) {
-        return new ModuleEntry(helper.extend(helper.clone(props), { packageName: packageName }));
-      });
-    });
-  };
-
-  ModuleEntryCollection.prototype.get = function(key) {
-    return this.data[key] || null;
-  };
-
-  ModuleEntryCollection.prototype.search = function(keyword) {
-    return helper.sortBy(helper.values(this.data).filter(function(moduleEntry) {
-      return (moduleEntry.keywordScore(keyword) !== 0);
-    }), function(moduleEntry) {
-      // sort in descending order
-      return -(moduleEntry.keywordScore(keyword));
-    });
-  };
 
   var Body = helper.inherits(function() {
     Body.super_.call(this, { element: dom.body() });
