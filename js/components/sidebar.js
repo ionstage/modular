a31d9fc6954434ce534a9533ee559bb8f93d698e
nasitra <nasitra@users.noreply.github.com>
2017-03-19 09:34:30 +0900

    Make the code of sidebar-content a separate file

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -1,90 +1,12 @@
 (function(app) {
   'use strict';
 
-  var IScroll = require('iscroll');
   var helper = app.helper || require('../helper.js');
   var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
+  var SidebarContent = app.SidebarContent || require('./sidebar-content.js');
   var SidebarHeader = app.SidebarHeader || require('./sidebar-header.js');
-  var SidebarModule = app.SidebarModule || require('./sidebar-module.js');
   var SidebarRelation = app.SidebarRelation || require('../relations/sidebar-relation.js');
-
-  var SidebarContent = helper.inherits(function(props) {
-    SidebarContent.super_.call(this, props);
-
-    this.modules = this.prop([]);
-
-    this.scrollable = new IScroll(this.element(), {
-      disableMouse: true,
-      disablePointer: true,
-      fadeScrollbars: dom.supportsTouch(),
-      interactiveScrollbars: !dom.supportsTouch(),
-      mouseWheel: true,
-      scrollbars: true,
-    });
-
-    this.dragStarter = props.dragStarter;
-    this.dragEnder = props.dragEnder;
-    this.dropper = props.dropper;
-
-    dom.on(this.element(), dom.eventType('start'), SidebarContent.prototype.onpoint.bind(this));
-  }, Component);
-
-  SidebarContent.prototype.scrollerElement = function() {
-    return dom.child(this.element(), 0);
-  };
-
-  SidebarContent.prototype.createModule = function(props) {
-    return new SidebarModule(helper.extend(helper.clone(props), {
-      dragStarter: this.dragStarter,
-      dragEnder: this.dragEnder,
-      dropper: this.dropper,
-    }));
-  };
-
-  SidebarContent.prototype.appendModule = function(props) {
-    var module = this.createModule(props);
-    this.modules().push(module);
-    module.parentElement(this.scrollerElement());
-    this.markDirty();
-  };
-
-  SidebarContent.prototype.clear = function() {
-    this.modules().forEach(function(module) {
-      module.delete();
-    });
-    this.modules([]);
-  };
-
-  SidebarContent.prototype.setModules = function(moduleEntries) {
-    this.clear();
-    moduleEntries.forEach(function(moduleEntry) {
-      this.appendModule({
-        title: moduleEntry.label,
-        content: moduleEntry.description,
-        name: moduleEntry.key(),
-      });
-    }.bind(this));
-  };
-
-  SidebarContent.prototype.scrollEnabled = function(enabled) {
-    if (enabled) {
-      this.scrollable.enable();
-    } else {
-      this.scrollable.disable();
-    }
-  };
-
-  SidebarContent.prototype.redraw = function() {
-    // XXX: zero timeout to wait for the repaint of iScroll
-    setTimeout(function() {
-      this.scrollable.refresh();
-    }.bind(this), 0);
-  };
-
-  SidebarContent.prototype.onpoint = function() {
-    dom.removeFocus();
-  };
 
   var Sidebar = helper.inherits(function(props) {
     Sidebar.super_.call(this, props);
