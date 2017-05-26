f68dabf38283b089eb51e9366f63d423d772656b
nasitra <nasitra@users.noreply.github.com>
2017-05-26 18:35:21 +0900

    Refactor method of updating lock-relation in terms of position

--- js/relations/lock-relation.js
+++ js/relations/lock-relation.js
@@ -10,30 +10,37 @@
     this.wire = props.wire;
   }, jCore.Relation);
 
-  LockRelation.prototype.positionType = (function() {
-    var map = { plug: 'source', socket: 'target' };
-    return function() {
-      return map[this.type];
-    };
-  })();
-
   LockRelation.prototype.equal = function(other) {
     return (!!other && Object.keys(this).every(function(key) {
       return helper.equal(this[key], other[key]);
     }.bind(this)));
   };
 
-  LockRelation.prototype.update = function() {
-    var wire = this.wire;
-    var positionType = this.positionType();
-    var position = this.unit[this.type + 'Position']();
+  LockRelation.prototype.unitPosition = function() {
+    var methodName = LockRelation.TYPE_POSITION_TABLE[this.type].methodName;
+    return this.unit[methodName]();
+  };
 
+  LockRelation.prototype.wirePosition = function(position) {
+    var wire = this.wire;
+    var positionType = LockRelation.TYPE_POSITION_TABLE[this.type].type;
     wire[positionType + 'X'](position.x);
     wire[positionType + 'Y'](position.y);
   };
 
+  LockRelation.prototype.update = function() {
+    this.wirePosition(this.unitPosition());
+  };
+
   LockRelation.TYPE_PLUG = 'plug';
   LockRelation.TYPE_SOCKET = 'socket';
+
+  LockRelation.TYPE_POSITION_TABLE = (function() {
+    var table = {};
+    table[LockRelation.TYPE_PLUG] = { methodName: 'plugPosition', type: 'source' };
+    table[LockRelation.TYPE_SOCKET] = { methodName: 'socketPosition', type: 'target' };
+    return table;
+  })();
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = LockRelation;
