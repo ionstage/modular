782995c14f2ef31dc1631abcdab6f0e3ceb7ae62
nasitra <nasitra@users.noreply.github.com>
2017-07-29 11:55:32 +0900

    Separate module-port into list-item and option

--- js/components/module-port.js
+++ js/components/module-port.js
@@ -5,50 +5,123 @@
   var Component = app.Component || require('./component.js');
 
   var ModulePort = Component.inherits(function(props) {
-    this.label = this.prop(props.label);
-    this.name = this.prop(props.name);
-    this.type = this.prop(props.type);
-    this.plugDisabled = this.prop(props.plugDisabled);
-    this.socketDisabled = this.prop(props.socketDisabled);
-    this.visible = this.prop(false);
-    this.top = this.prop(0);
-    this.highlighted = this.prop(false);
-    this.isMoving = this.prop(false);
-    this.plugHighlighted = this.prop(false);
-    this.socketHighlighted = this.prop(false);
-    this.socketConnected = this.prop(false);
-    this.height = this.prop(44);
-    this.plugOffsetX = this.prop(261);
-    this.socketOffsetX = this.prop(-25);
-    this.listItemElement = this.prop(this.renderListItem());
-    this.parentListElement = this.prop(props.parentListElement);
-    this.optionElement = this.prop(this.renderOption());
-    this.parentOptGroupElement = this.prop(props.parentOptGroupElement);
+    this.listItem = new ModulePort.ListItem({
+      element: this.renderListItem(),
+      parentElement: props.parentListElement,
+      visible: false,
+      label: props.label,
+      type: props.type,
+      top: 0,
+      highlighted: false,
+      isMoving: false,
+      plugDisabled: props.plugDisabled,
+      plugHighlighted: false,
+      socketDisabled: props.socketDisabled,
+      socketHighlighted: false,
+      socketConnected: false,
+    });
+
+    this.option = new ModulePort.Option({
+      element: this.renderOption(),
+      parentElement: props.parentOptGroupElement,
+      visible: true,
+      label: props.label,
+      name: props.name,
+    });
   });
 
-  ModulePort.prototype.plugElement = function() {
-    return dom.child(this.listItemElement(), 0);
+  ModulePort.prototype.listItemElement = function() {
+    return this.listItem.element();
   };
 
-  ModulePort.prototype.socketElement = function() {
-    return dom.child(this.listItemElement(), 1);
+  ModulePort.prototype.visible = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+      this.option.visible(!value);
+    }
+    return this.listItem.visible(value);
   };
 
-  ModulePort.prototype.socketHandleElement = function() {
-    return dom.child(this.listItemElement(), 1, 0);
+  ModulePort.prototype.label = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    this.option.label(value);
+    return this.listItem.label(value);
   };
 
-  ModulePort.prototype.labelElement = function() {
-    return dom.child(this.listItemElement(), 2);
+  ModulePort.prototype.name = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.option.name(value);
   };
 
-  ModulePort.prototype.hideButtonElement = function() {
-    return dom.child(this.listItemElement(), 3);
+  ModulePort.prototype.type = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.type(value);
   };
 
-  ModulePort.prototype.hideDisabled = function() {
-    // don't hide highlighted port
-    return this.highlighted();
+  ModulePort.prototype.top = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.top(value);
+  };
+
+  ModulePort.prototype.highlighted = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.highlighted(value);
+  };
+
+  ModulePort.prototype.isMoving = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.isMoving(value);
+  };
+
+  ModulePort.prototype.plugDisabled = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.plugDisabled(value);
+  };
+
+  ModulePort.prototype.plugHighlighted = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.plugHighlighted(value);
+  };
+
+  ModulePort.prototype.socketDisabled = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.socketDisabled(value);
+  };
+
+  ModulePort.prototype.socketHighlighted = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.socketHighlighted(value);
+  };
+
+  ModulePort.prototype.socketConnected = function(value) {
+    if (typeof value !== 'undefined') {
+      this.markDirty();
+    }
+    return this.listItem.socketConnected(value);
+  };
+
+  ModulePort.prototype.height = function() {
+    return this.listItem.height();
   };
 
   ModulePort.prototype.middle = function() {
@@ -57,6 +130,14 @@
 
   ModulePort.prototype.bottom = function() {
     return this.top() + this.height();
+  };
+
+  ModulePort.prototype.plugOffsetX = function() {
+    return this.listItem.plugOffsetX();
+  };
+
+  ModulePort.prototype.socketOffsetX = function() {
+    return this.listItem.socketOffsetX();
   };
 
   ModulePort.prototype.renderListItem = function() {
@@ -68,87 +149,8 @@
   };
 
   ModulePort.prototype.redraw = function() {
-    this.redrawVisibility();
-    this.redrawLabel();
-    this.redrawName();
-    this.redrawListItem();
-    this.redrawPlug();
-    this.redrawSocket();
-    this.redrawHideButton();
-  };
-
-  ModulePort.prototype.redrawVisibility = function() {
-    this.redrawProp('visible', function(visible) {
-      if (visible) {
-        dom.remove(this.optionElement());
-        dom.append(this.parentListElement(), this.listItemElement());
-      } else {
-        dom.remove(this.listItemElement());
-        dom.append(this.parentOptGroupElement(), this.optionElement());
-      }
-    });
-  };
-
-  ModulePort.prototype.redrawLabel = function() {
-    this.redrawProp('label', function(label) {
-      dom.text(this.labelElement(), label);
-      dom.text(this.optionElement(), label);
-    });
-  };
-
-  ModulePort.prototype.redrawName = function() {
-    this.redrawProp('name', function(name) {
-      dom.value(this.optionElement(), name);
-    });
-  };
-
-  ModulePort.prototype.redrawListItem = function() {
-    this.redrawProp('type', function(type) {
-      dom.data(this.listItemElement(), 'type', type);
-    });
-
-    this.redrawProp('top', function(top) {
-      dom.translateY(this.listItemElement(), top);
-    });
-
-    this.redrawProp('highlighted', function(highlighted) {
-      dom.toggleClass(this.listItemElement(), 'highlighted', highlighted);
-    });
-
-    this.redrawProp('isMoving', function(isMoving) {
-      dom.toggleClass(this.listItemElement(), 'moving', isMoving);
-    });
-  };
-
-  ModulePort.prototype.redrawPlug = function() {
-    this.redrawProp('plugDisabled', function(plugDisabled) {
-      dom.toggleClass(this.plugElement(), 'hide', plugDisabled);
-    });
-
-    this.redrawProp('plugHighlighted', function(plugHighlighted) {
-      dom.toggleClass(this.plugElement(), 'highlighted', plugHighlighted);
-    });
-  };
-
-  ModulePort.prototype.redrawSocket = function() {
-    this.redrawProp('socketDisabled', function(socketDisabled) {
-      dom.toggleClass(this.socketElement(), 'hide', socketDisabled);
-    });
-
-    this.redrawProp('socketHighlighted', function(socketHighlighted) {
-      dom.toggleClass(this.socketElement(), 'highlighted', socketHighlighted);
-      dom.toggleClass(this.socketHandleElement(), 'highlighted', socketHighlighted);
-    });
-
-    this.redrawProp('socketConnected', function(socketConnected) {
-      dom.toggleClass(this.socketHandleElement(), 'hide', !socketConnected);
-    });
-  };
-
-  ModulePort.prototype.redrawHideButton = function() {
-    this.redrawProp('hideDisabled', function(hideDisabled) {
-      dom.toggleClass(this.hideButtonElement(), 'disabled', hideDisabled);
-    });
+    this.listItem.redraw();
+    this.option.redraw();
   };
 
   ModulePort.TYPE_PROP = 'prop';
@@ -169,6 +171,164 @@
   ].join('');
 
   ModulePort.OPTION_HTML_TEXT = '<option></option>';
+
+  ModulePort.ListItem = (function() {
+    var ListItem = Component.inherits(function(props) {
+      this.visible = this.prop(props.visible);
+      this.label = this.prop(props.label);
+      this.type = this.prop(props.type);
+      this.top = this.prop(props.top);
+      this.highlighted = this.prop(props.highlighted);
+      this.isMoving = this.prop(props.isMoving);
+      this.plugDisabled = this.prop(props.plugDisabled);
+      this.plugHighlighted = this.prop(props.plugHighlighted);
+      this.socketDisabled = this.prop(props.socketDisabled);
+      this.socketHighlighted = this.prop(props.socketHighlighted);
+      this.socketConnected = this.prop(props.socketConnected);
+      this.height = this.prop(44);
+      this.plugOffsetX = this.prop(261);
+      this.socketOffsetX = this.prop(-25);
+    });
+
+    ListItem.prototype.plugElement = function() {
+      return dom.child(this.element(), 0);
+    };
+
+    ListItem.prototype.socketElement = function() {
+      return dom.child(this.element(), 1);
+    };
+
+    ListItem.prototype.socketHandleElement = function() {
+      return dom.child(this.element(), 1, 0);
+    };
+
+    ListItem.prototype.labelElement = function() {
+      return dom.child(this.element(), 2);
+    };
+
+    ListItem.prototype.hideButtonElement = function() {
+      return dom.child(this.element(), 3);
+    };
+
+    ListItem.prototype.hideDisabled = function() {
+      // don't hide highlighted port
+      return this.highlighted();
+    };
+
+    ListItem.prototype.onredraw = function() {
+      this.redrawVisibility();
+      this.redrawLabel();
+      this.redrawContent();
+      this.redrawPlug();
+      this.redrawSocket();
+      this.redrawHideButton();
+    };
+
+    ListItem.prototype.redrawVisibility = function() {
+      this.redrawProp('visible', function(visible) {
+        if (visible) {
+          dom.append(this.parentElement(), this.element());
+        } else {
+          dom.remove(this.element());
+        }
+      });
+    };
+
+    ListItem.prototype.redrawLabel = function() {
+      this.redrawProp('label', function(label) {
+        dom.text(this.labelElement(), label);
+      });
+    };
+
+    ListItem.prototype.redrawContent = function() {
+      this.redrawProp('type', function(type) {
+        dom.data(this.element(), 'type', type);
+      });
+
+      this.redrawProp('top', function(top) {
+        dom.translateY(this.element(), top);
+      });
+
+      this.redrawProp('highlighted', function(highlighted) {
+        dom.toggleClass(this.element(), 'highlighted', highlighted);
+      });
+
+      this.redrawProp('isMoving', function(isMoving) {
+        dom.toggleClass(this.element(), 'moving', isMoving);
+      });
+    };
+
+    ListItem.prototype.redrawPlug = function() {
+      this.redrawProp('plugDisabled', function(plugDisabled) {
+        dom.toggleClass(this.plugElement(), 'hide', plugDisabled);
+      });
+
+      this.redrawProp('plugHighlighted', function(plugHighlighted) {
+        dom.toggleClass(this.plugElement(), 'highlighted', plugHighlighted);
+      });
+    };
+
+    ListItem.prototype.redrawSocket = function() {
+      this.redrawProp('socketDisabled', function(socketDisabled) {
+        dom.toggleClass(this.socketElement(), 'hide', socketDisabled);
+      });
+
+      this.redrawProp('socketHighlighted', function(socketHighlighted) {
+        dom.toggleClass(this.socketElement(), 'highlighted', socketHighlighted);
+        dom.toggleClass(this.socketHandleElement(), 'highlighted', socketHighlighted);
+      });
+
+      this.redrawProp('socketConnected', function(socketConnected) {
+        dom.toggleClass(this.socketHandleElement(), 'hide', !socketConnected);
+      });
+    };
+
+    ListItem.prototype.redrawHideButton = function() {
+      this.redrawProp('hideDisabled', function(hideDisabled) {
+        dom.toggleClass(this.hideButtonElement(), 'disabled', hideDisabled);
+      });
+    };
+
+    return ListItem;
+  })();
+
+  ModulePort.Option = (function() {
+    var Option = Component.inherits(function(props) {
+      this.visible = this.prop(props.visible);
+      this.label = this.prop(props.label);
+      this.name = this.prop(props.name);
+    });
+
+    Option.prototype.onredraw = function() {
+      this.redrawVisibility();
+      this.redrawLabel();
+      this.redrawName();
+    };
+
+    Option.prototype.redrawVisibility = function() {
+      this.redrawProp('visible', function(visible) {
+        if (visible) {
+          dom.append(this.parentElement(), this.element());
+        } else {
+          dom.remove(this.element());
+        }
+      });
+    };
+
+    Option.prototype.redrawLabel = function() {
+      this.redrawProp('label', function(label) {
+        dom.text(this.element(), label);
+      });
+    };
+
+    Option.prototype.redrawName = function() {
+      this.redrawProp('name', function(name) {
+        dom.value(this.element(), name);
+      });
+    };
+
+    return Option;
+  })();
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = ModulePort;
