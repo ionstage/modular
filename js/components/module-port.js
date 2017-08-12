562c51670d3518f286c523747d9ed30676edbae7
nasitra <nasitra@users.noreply.github.com>
2017-08-12 11:25:59 +0900

    Refactor code of module-port as its list-item

--- js/components/module-port.js
+++ js/components/module-port.js
@@ -5,15 +5,21 @@
   var Component = app.Component || require('./component.js');
 
   var ModulePort = Component.inherits(function(props) {
+    this.type = this.prop(props.type);
+    this.top = this.prop(0);
+    this.isMoving = this.prop(false);
+    this.height = this.prop(44);
+    this.plugOffsetX = this.prop(261);
+    this.socketOffsetX = this.prop(-25);
     this.parentListElement = this.prop(props.parentListElement);
     this.parentOptGroupElement = this.prop(props.parentOptGroupElement);
 
-    this.listItem = new ModulePort.ListItem({
-      label: props.label,
-      type: props.type,
-      plugDisabled: props.plugDisabled,
-      socketDisabled: props.socketDisabled,
-    });
+    this.plug = new ModulePort.Plug({ disabled: props.plugDisabled });
+    this.socket = new ModulePort.Socket({ disabled: props.socketDisabled });
+    this.content = new ModulePort.Content({ label: props.label });
+    this.hideButton = new ModulePort.HideButton();
+
+    this.children = [this.plug, this.socket, this.content, this.hideButton];
 
     this.option = new ModulePort.Option({
       parentElement: props.parentOptGroupElement,
@@ -24,51 +30,34 @@
 
   ModulePort.prototype.visible = function(value) {
     if (typeof value !== 'undefined') {
-      this.markDirty();
-      this.listItem.parentElement(value ? this.parentListElement() : null);
+      this.parentElement(value ? this.parentListElement() : null);
       this.option.parentElement(value ? null : this.parentOptGroupElement());
     }
-    return (this.listItem.parentElement() !== null);
-  };
-
-  ModulePort.prototype.label = function() {
-    return this.listItem.label();
+    return (this.parentElement() !== null);
   };
 
   ModulePort.prototype.name = function() {
     return this.option.name();
   };
 
-  ModulePort.prototype.type = function() {
-    return this.listItem.type();
-  };
-
-  ModulePort.prototype.top = function(value) {
-    if (typeof value !== 'undefined') {
-      this.markDirty();
-    }
-    return this.listItem.top(value);
-  };
-
   ModulePort.prototype.highlighted = function(value) {
     if (typeof value !== 'undefined') {
       this.markDirty();
-
-      // don't hide highlighted port
-      this.listItem.hideDisabled(value);
     }
-    return this.listItem.highlighted(value);
+    // don't hide highlighted port
+    return this.hideButton.disabled(value);
   };
 
-  ModulePort.prototype.isMoving = function(value) {
-    if (typeof value !== 'undefined') {
-      this.markDirty();
-    }
-    return this.listItem.isMoving(value);
+  ModulePort.prototype.plugDisabled = function(value) {
+    return this.plug.disabled(value);
   };
 
-  ModulePort.prototype.plugDisabled = function() {
-    return this.listItem.plugDisabled();
+  ModulePort.prototype.socketDisabled = function(value) {
+    return this.socket.disabled(value);
+  };
+
+  ModulePort.prototype.label = function(value) {
+    return this.content.label(value);
   };
 
   ModulePort.prototype.plugHighlighted = function(value) {
@@ -78,29 +67,21 @@
     if (typeof value !== 'undefined') {
       this.markDirty();
     }
-    return this.listItem.plugHighlighted(value);
-  };
-
-  ModulePort.prototype.socketDisabled = function() {
-    return this.listItem.socketDisabled();
+    return this.plug.highlighted(value);
   };
 
   ModulePort.prototype.socketHighlighted = function(value) {
     if (typeof value !== 'undefined') {
       this.markDirty();
     }
-    return this.listItem.socketHighlighted(value);
+    return this.socket.highlighted(value);
   };
 
   ModulePort.prototype.socketConnected = function(value) {
     if (typeof value !== 'undefined') {
       this.markDirty();
     }
-    return this.listItem.socketConnected(value);
-  };
-
-  ModulePort.prototype.height = function() {
-    return this.listItem.height();
+    return this.socket.connected(value);
   };
 
   ModulePort.prototype.middle = function() {
@@ -111,22 +92,25 @@
     return this.top() + this.height();
   };
 
-  ModulePort.prototype.plugOffsetX = function() {
-    return this.listItem.plugOffsetX();
+  ModulePort.prototype.elementContains = function(target) {
+    var element = this.element();
+    return (element ? dom.contains(element, target) : false);
   };
 
-  ModulePort.prototype.socketOffsetX = function() {
-    return this.listItem.socketOffsetX();
-  };
-
-  ModulePort.prototype.elementContains = function(element) {
-    var listItemElement = this.listItem.element();
-    return (listItemElement ? dom.contains(listItemElement, element) : false);
+  ModulePort.prototype.render = function() {
+    return dom.render(ModulePort.HTML_TEXT);
   };
 
   ModulePort.prototype.redraw = function() {
-    this.listItem.redraw();
+    ModulePort.super_.prototype.redraw.call(this);
     this.option.redraw();
+  };
+
+  ModulePort.prototype.onredraw = function() {
+    this.redrawDOMDataBy('type', 'type');
+    this.redrawDOMTranslateYBy('top');
+    this.redrawDOMToggleClassBy('highlighted', 'highlighted');
+    this.redrawDOMToggleClassBy('isMoving', 'moving');
   };
 
   ModulePort.TYPE_PROP = 'prop';
@@ -135,175 +119,115 @@
   ModulePort.PLUG_WIDTH = 50;
   ModulePort.SOCKET_WIDTH = 50;
 
-  ModulePort.ListItem = (function() {
-    var ListItem = Component.inherits(function(props) {
-      this.type = this.prop(props.type);
-      this.top = this.prop(0);
+  ModulePort.HTML_TEXT = '<div class="module-port"></div>';
+
+  ModulePort.Handle = (function() {
+    var Handle = Component.inherits(function(props) {
+      this.disabled = this.prop(props.disabled);
       this.highlighted = this.prop(false);
-      this.isMoving = this.prop(false);
-      this.height = this.prop(44);
-      this.plugOffsetX = this.prop(261);
-      this.socketOffsetX = this.prop(-25);
-
-      this.plug = new ListItem.Plug({ disabled: props.plugDisabled });
-      this.socket = new ListItem.Socket({ disabled: props.socketDisabled });
-      this.content = new ListItem.Content({ label: props.label });
-      this.hideButton = new ListItem.HideButton();
-
-      this.children = [this.plug, this.socket, this.content, this.hideButton];
     });
 
-    ListItem.prototype.plugDisabled = function(value) {
-      return this.plug.disabled(value);
-    };
-
-    ListItem.prototype.plugHighlighted = function(value) {
-      return this.plug.highlighted(value);
-    };
-
-    ListItem.prototype.socketDisabled = function(value) {
-      return this.socket.disabled(value);
-    };
-
-    ListItem.prototype.socketHighlighted = function(value) {
-      return this.socket.highlighted(value);
-    };
-
-    ListItem.prototype.socketConnected = function(value) {
-      return this.socket.connected(value);
-    };
-
-    ListItem.prototype.label = function(value) {
-      return this.content.label(value);
-    };
-
-    ListItem.prototype.hideDisabled = function(value) {
-      return this.hideButton.disabled(value);
-    };
-
-    ListItem.prototype.render = function() {
-      return dom.render(ListItem.HTML_TEXT);
-    };
-
-    ListItem.prototype.onredraw = function() {
-      this.redrawDOMDataBy('type', 'type');
-      this.redrawDOMTranslateYBy('top');
+    Handle.prototype.onredraw = function() {
+      this.redrawDOMToggleClassBy('disabled', 'hide');
       this.redrawDOMToggleClassBy('highlighted', 'highlighted');
-      this.redrawDOMToggleClassBy('isMoving', 'moving');
     };
 
-    ListItem.HTML_TEXT = '<div class="module-port"></div>';
+    return Handle;
+  })();
 
-    ListItem.Handle = (function() {
-      var Handle = Component.inherits(function(props) {
-        this.disabled = this.prop(props.disabled);
-        this.highlighted = this.prop(false);
-      });
+  ModulePort.Plug = (function() {
+    var Plug = ModulePort.Handle.inherits();
 
-      Handle.prototype.onredraw = function() {
-        this.redrawDOMToggleClassBy('disabled', 'hide');
-        this.redrawDOMToggleClassBy('highlighted', 'highlighted');
-      };
+    Plug.prototype.render = function() {
+      return dom.render(Plug.HTML_TEXT);
+    };
 
-      return Handle;
-    })();
+    Plug.HTML_TEXT = '<div class="module-port-plug module-port-handle"></div>';
 
-    ListItem.Plug = (function() {
-      var Plug = ListItem.Handle.inherits();
+    return Plug;
+  })();
 
-      Plug.prototype.render = function() {
-        return dom.render(Plug.HTML_TEXT);
-      };
+  ModulePort.Socket = (function() {
+    var Socket = Component.inherits(function(props) {
+      this.disabled = this.prop(props.disabled);
+      this.handle = new ModulePort.SocketHandle({ disabled: true });
+      this.children = [this.handle];
+    });
 
-      Plug.HTML_TEXT = '<div class="module-port-plug module-port-handle"></div>';
+    Socket.prototype.highlighted = function(value) {
+      if (typeof value !== 'undefined') {
+        this.markDirty();
+      }
+      return this.handle.highlighted(value);
+    };
 
-      return Plug;
-    })();
+    Socket.prototype.connected = function(value) {
+      if (typeof value === 'undefined') {
+        return !this.handle.disabled();
+      }
+      this.handle.disabled(!value);
+    };
 
-    ListItem.Socket = (function() {
-      var Socket = Component.inherits(function(props) {
-        this.disabled = this.prop(props.disabled);
-        this.handle = new ListItem.SocketHandle({ disabled: true });
-        this.children = [this.handle];
-      });
+    Socket.prototype.render = function() {
+      return dom.render(Socket.HTML_TEXT);
+    };
 
-      Socket.prototype.highlighted = function(value) {
-        if (typeof value !== 'undefined') {
-          this.markDirty();
-        }
-        return this.handle.highlighted(value);
-      };
+    Socket.prototype.onredraw = function() {
+      this.redrawDOMToggleClassBy('disabled', 'hide');
+      this.redrawDOMToggleClassBy('highlighted', 'highlighted');
+    };
 
-      Socket.prototype.connected = function(value) {
-        if (typeof value === 'undefined') {
-          return !this.handle.disabled();
-        }
-        this.handle.disabled(!value);
-      };
+    Socket.HTML_TEXT = '<div class="module-port-socket"></div>';
 
-      Socket.prototype.render = function() {
-        return dom.render(Socket.HTML_TEXT);
-      };
+    return Socket;
+  })();
 
-      Socket.prototype.onredraw = function() {
-        this.redrawDOMToggleClassBy('disabled', 'hide');
-        this.redrawDOMToggleClassBy('highlighted', 'highlighted');
-      };
+  ModulePort.SocketHandle = (function() {
+    var SocketHandle = ModulePort.Handle.inherits();
 
-      Socket.HTML_TEXT = '<div class="module-port-socket"></div>';
+    SocketHandle.prototype.render = function() {
+      return dom.render(SocketHandle.HTML_TEXT);
+    };
 
-      return Socket;
-    })();
+    SocketHandle.HTML_TEXT = '<div class="module-port-socket-handle module-port-handle"></div>';
 
-    ListItem.SocketHandle = (function() {
-      var SocketHandle = ListItem.Handle.inherits();
+    return SocketHandle;
+  })();
 
-      SocketHandle.prototype.render = function() {
-        return dom.render(SocketHandle.HTML_TEXT);
-      };
+  ModulePort.Content = (function() {
+    var Content = Component.inherits(function(props) {
+      this.label = this.prop(props.label);
+    });
 
-      SocketHandle.HTML_TEXT = '<div class="module-port-socket-handle module-port-handle"></div>';
+    Content.prototype.render = function() {
+      return dom.render(Content.HTML_TEXT);
+    };
 
-      return SocketHandle;
-    })();
+    Content.prototype.onredraw = function() {
+      this.redrawDOMTextBy('label');
+    };
 
-    ListItem.Content = (function() {
-      var Content = Component.inherits(function(props) {
-        this.label = this.prop(props.label);
-      });
+    Content.HTML_TEXT = '<div class="module-port-content"></div>';
 
-      Content.prototype.render = function() {
-        return dom.render(Content.HTML_TEXT);
-      };
+    return Content;
+  })();
 
-      Content.prototype.onredraw = function() {
-        this.redrawDOMTextBy('label');
-      };
+  ModulePort.HideButton = (function() {
+    var HideButton = Component.inherits(function() {
+      this.disabled = this.prop(false);
+    });
 
-      Content.HTML_TEXT = '<div class="module-port-content"></div>';
+    HideButton.prototype.render = function() {
+      return dom.render(HideButton.HTML_TEXT);
+    };
 
-      return Content;
-    })();
+    HideButton.prototype.onredraw = function() {
+      this.redrawDOMToggleClassBy('disabled', 'disabled');
+    };
 
-    ListItem.HideButton = (function() {
-      var HideButton = Component.inherits(function() {
-        this.disabled = this.prop(false);
-      });
+    HideButton.HTML_TEXT = '<img class="module-port-hide-button" src="images/minus-square-o.svg">';
 
-      HideButton.prototype.render = function() {
-        return dom.render(HideButton.HTML_TEXT);
-      };
-
-      HideButton.prototype.onredraw = function() {
-        this.redrawDOMToggleClassBy('disabled', 'disabled');
-      };
-
-      HideButton.HTML_TEXT = '<img class="module-port-hide-button" src="images/minus-square-o.svg">';
-
-      return HideButton;
-    })();
-
-    return ListItem;
+    return HideButton;
   })();
 
   ModulePort.Option = (function() {
