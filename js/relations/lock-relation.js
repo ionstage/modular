01daaa9dd38e471f722419edc0fd3ebacfe9af43
nasitra <nasitra@users.noreply.github.com>
2018-01-19 19:34:05 +0900

    Refactor 'update' method of lock-relation

--- js/relations/lock-relation.js
+++ js/relations/lock-relation.js
@@ -7,24 +7,26 @@
     this.type = props.type;
     this.port = props.port;
     this.wire = props.wire;
+    this.updater = LockRelation.UPDATERS[this.type];
   });
 
   LockRelation.prototype.update = function() {
-    var x = this.port[this.type.portKeys.x]();
-    var y = this.port[this.type.portKeys.y]();
-    this.wire[this.type.wireKeys.x](x);
-    this.wire[this.type.wireKeys.y](y);
+    this.updater(this.port, this.wire);
   };
 
-  LockRelation.TYPE_PLUG = {
-    portKeys: { x: 'plugX', y: 'plugY' },
-    wireKeys: { x: 'sourceX', y: 'sourceY' },
-  };
+  LockRelation.UPDATERS = [
+    function(port, wire) {
+      wire.sourceX(port.plugX());
+      wire.sourceY(port.plugY());
+    },
+    function(port, wire) {
+      wire.targetX(port.socketX());
+      wire.targetY(port.socketY());
+    },
+  ];
 
-  LockRelation.TYPE_SOCKET = {
-    portKeys: { x: 'socketX', y: 'socketY' },
-    wireKeys: { x: 'targetX', y: 'targetY' },
-  };
+  LockRelation.TYPE_PLUG = 0;
+  LockRelation.TYPE_SOCKET = 1;
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = LockRelation;
