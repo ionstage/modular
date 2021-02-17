b35bcdf6ccf9cefa477656a65d6ed199f2a5c2ae
nasitra <nasitra@users.noreply.github.com>
2021-02-17 18:57:30 +0900

    Add utility code for modular-module

---
+++ modular_modules/modular-util.js
@@ -0,0 +1,45 @@
+(function(window) {
+  'use strict';
+
+  var util = {};
+
+  util.isNumber = function(obj) {
+    return (Object.prototype.toString.call(obj) === '[object Number]');
+  };
+
+  util.toNumber = function(value) {
+    if (value == null) {
+      return NaN;
+    }
+    if (util.isNumber(value)) {
+      return value;
+    }
+    try {
+      value = JSON.parse(String(value));
+      return (util.isNumber(value) ? value : NaN);
+    } catch (e) {
+      return NaN;
+    }
+  };
+
+  util.debounce = function(func, delay) {
+    var t = 0;
+    var ctx = null;
+    var args = null;
+    return function() {
+      ctx = this;
+      args = arguments;
+      if (t) {
+        clearTimeout(t);
+      }
+      t = setTimeout(function() {
+        func.apply(ctx, args);
+        t = 0;
+        ctx = null;
+        args = null;
+      }, delay);
+    };
+  };
+
+  Object.defineProperty(window.modular, 'util', { value: util });
+})(this);
