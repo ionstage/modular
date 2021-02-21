d12a175edb4a94ce8bfc20ae5080cb62bf4f41b5
nasitra <nasitra@users.noreply.github.com>
2021-02-21 17:03:12 +0900

    Modify code for updating prop values

--- modular_modules/modular-util.js
+++ modular_modules/modular-util.js
@@ -32,24 +32,5 @@
     };
   };
 
-  util.debounce = function(func, delay) {
-    var t = 0;
-    var ctx = null;
-    var args = null;
-    return function() {
-      ctx = this;
-      args = arguments;
-      if (t) {
-        clearTimeout(t);
-      }
-      t = setTimeout(function() {
-        func.apply(ctx, args);
-        t = 0;
-        ctx = null;
-        args = null;
-      }, delay);
-    };
-  };
-
   Object.defineProperty(window.modular, 'util', { value: util });
 })(this);
