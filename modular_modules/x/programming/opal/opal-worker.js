36c0b43c18d0a3c1028e5e7c92115a5e4f41e7eb
nasitra <nasitra@users.noreply.github.com>
2021-02-28 17:47:15 +0900

    Use 'Web Worker' to execute ruby code

---
+++ modular_modules/x/programming/opal/opal-worker.js
@@ -0,0 +1,57 @@
+if (typeof globalThis === 'undefined') {
+  // define `globalThis` for IE
+  self.globalThis = self;
+}
+
+self.importScripts('opal.min.js');
+
+// disable network access
+self.importScripts = null;
+self.XMLHttpRequest = null;
+self.fetch = null;
+
+var Opal = self.Opal;
+
+var input = null;
+var output = '';
+var error = '';
+
+Opal.global.console.log = function(s) {
+  output += s;
+};
+
+Opal.global.console.warn = function(s) {
+  error += s;
+};
+
+Opal.STDIN.$read = function() {
+  if (arguments.length !== 0) {
+    throw 'read: not implemented';
+  }
+  return (input !== null ? input : Opal.nil);
+};
+
+self.addEventListener('message', function(event) {
+  var data = event.data;
+
+  input = data.input;
+  output = '';
+  error = '';
+
+  try {
+    Opal.eval(data.code);
+  } catch (e) {
+    self.postMessage({
+      output: null,
+      error: null,
+      exception: e.toString() || 'undefined exception',
+    });
+    return;
+  }
+
+  self.postMessage({
+    output: output,
+    error: error,
+    exception: '',
+  });
+}, false);
