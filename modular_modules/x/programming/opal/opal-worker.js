4b85097da2acd38c23518e6db1cf7fb3fbf51c96
nasitra <nasitra@users.noreply.github.com>
2021-03-02 21:25:10 +0900

    Make local variables for 'opal-worker'

--- modular_modules/x/programming/opal/opal-worker.js
+++ modular_modules/x/programming/opal/opal-worker.js
@@ -11,48 +11,48 @@ self.XMLHttpRequest = null;
 self.fetch = null;
 self.WebSocket = null;
 
-var Opal = self.Opal;
+self.addEventListener('message', (function() {
+  var input = null;
+  var output = '';
+  var error = '';
 
-var input = null;
-var output = '';
-var error = '';
+  self.Opal.global.console.log = function(s) {
+    output += s;
+  };
 
-Opal.global.console.log = function(s) {
-  output += s;
-};
+  self.Opal.global.console.warn = function(s) {
+    error += s;
+  };
 
-Opal.global.console.warn = function(s) {
-  error += s;
-};
+  self.Opal.STDIN.$read = function() {
+    if (arguments.length !== 0) {
+      throw 'read: not implemented';
+    }
+    return (input !== null ? input : self.Opal.nil);
+  };
 
-Opal.STDIN.$read = function() {
-  if (arguments.length !== 0) {
-    throw 'read: not implemented';
-  }
-  return (input !== null ? input : Opal.nil);
-};
+  return function(event) {
+    var data = event.data;
 
-self.addEventListener('message', function(event) {
-  var data = event.data;
+    input = data.input;
+    output = '';
+    error = '';
 
-  input = data.input;
-  output = '';
-  error = '';
+    try {
+      self.Opal.eval(data.code);
+    } catch (e) {
+      self.postMessage({
+        output: null,
+        error: null,
+        exception: e.toString() || 'undefined exception',
+      });
+      return;
+    }
 
-  try {
-    Opal.eval(data.code);
-  } catch (e) {
     self.postMessage({
-      output: null,
-      error: null,
-      exception: e.toString() || 'undefined exception',
+      output: output,
+      error: error,
+      exception: '',
     });
-    return;
-  }
-
-  self.postMessage({
-    output: output,
-    error: error,
-    exception: '',
-  });
-}, false);
+  };
+})(), false);
