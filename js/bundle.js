cff97b00eb7227ce734d6f08bce41e2952a8ae08
nasitra <nasitra@users.noreply.github.com>
2021-03-19 18:34:45 +0900

    Update 'circuit' module to the latest version

--- js/bundle.js
+++ js/bundle.js
@@ -186,7 +186,7 @@ process.umask = function() { return 0; };
 
 },{}],"circuit":[function(require,module,exports){
 /**
- * circuit v1.1.3
+ * circuit v1.2.0
  * (c) 2015 iOnStage
  * Released under the MIT License.
  */
@@ -217,7 +217,7 @@ process.umask = function() { return 0; };
 
   var circuit = {};
 
-  var CircuitProp = function(initialValue) {
+  var CircuitData = function(initialValue) {
     var self = this;
 
     var func = function() {
@@ -259,7 +259,7 @@ process.umask = function() { return 0; };
     return func;
   };
 
-  CircuitProp.prototype.update = function() {
+  CircuitData.prototype.update = function() {
     if (!this.dirty)
       return;
 
@@ -277,13 +277,13 @@ process.umask = function() { return 0; };
     this.markDirty();
   };
 
-  CircuitProp.prototype.updateCache = function(value) {
+  CircuitData.prototype.updateCache = function(value) {
     this.cache = value;
     this.dirty = false;
   };
 
-  CircuitProp.prototype.markDirty = function() {
-    CircuitProp.markDirtyTargets(this.targets);
+  CircuitData.prototype.markDirty = function() {
+    CircuitData.markDirtyTargets(this.targets);
 
     if (!this.dirty)
       return;
@@ -300,7 +300,7 @@ process.umask = function() { return 0; };
     }
   };
 
-  CircuitProp.markDirtyTargets = (function() {
+  CircuitData.markDirtyTargets = (function() {
     var dirtyTargets = [];
     var timer = null;
 
@@ -317,7 +317,7 @@ process.umask = function() { return 0; };
         if (lastIndexOf(dirtyTargets, target) === -1)
           dirtyTargets.push(target);
 
-        CircuitProp.markDirtyTargets(targetSelf.targets);
+        CircuitData.markDirtyTargets(targetSelf.targets);
       }
 
       if (timer !== null)
@@ -350,7 +350,7 @@ process.umask = function() { return 0; };
       if (typeof context === 'undefined')
         context = null;
 
-      var contextProp = function(value) {
+      var contextData = function(value) {
         if (typeof value === 'undefined')
           return context;
         context = value;
@@ -361,9 +361,9 @@ process.umask = function() { return 0; };
           canceled = true;
         },
         dispatch: function() {
-          self.dispatch(contextProp());
+          self.dispatch(contextData());
         },
-        context: contextProp
+        context: contextData
       };
 
       if (typeof listener === 'function')
@@ -372,7 +372,7 @@ process.umask = function() { return 0; };
       if (canceled)
         return;
 
-      self.dispatch(contextProp());
+      self.dispatch(contextData());
     };
 
     func._self = self;
@@ -393,9 +393,12 @@ process.umask = function() { return 0; };
     }, 0);
   };
 
-  circuit.prop = function(initialValue) {
-    return new CircuitProp(initialValue);
+  circuit.data = function(initialValue) {
+    return new CircuitData(initialValue);
   };
+
+  // alias for backward compatibility
+  circuit.prop = circuit.data;
 
   circuit.event = function(listener) {
     return new CircuitEvent(listener);
@@ -409,13 +412,13 @@ process.umask = function() { return 0; };
     var targetSelf = target._self;
 
     if (sourceSelf.constructor !== targetSelf.constructor)
-      throw new TypeError('Cannot bind prop and event');
+      throw new TypeError('Cannot bind data and event');
 
     sourceSelf.targets.push(target);
     targetSelf.sources.push(source);
 
-    if (sourceSelf.constructor === CircuitProp) {
-      CircuitProp.markDirtyTargets([target]);
+    if (sourceSelf.constructor === CircuitData) {
+      CircuitData.markDirtyTargets([target]);
       source();
     }
   };
@@ -432,7 +435,7 @@ process.umask = function() { return 0; };
     if (targetIndex === -1)
       throw new Error('Already unbound');
 
-    if (targetSelf.constructor === CircuitProp && targetSelf.dirty)
+    if (targetSelf.constructor === CircuitData && targetSelf.dirty)
       target();
 
     var sourceIndex = lastIndexOf(targetSelf.sources, source);
